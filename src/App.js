5c5a07b23809969a05d9d782fbcc36f01c53c02a
BhoomiSarariya <bhoomi5566@gmail.com>
2020-02-19 10:55:29 +0530

    form validation common component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import Input from './Input';
+import Input, { RadioButton, DropDown, SelectOneCheckBox, CheckBox } from './Input';
 import { Row, Col, Button, Form } from 'react-bootstrap';
 
 class App extends React.Component {
@@ -12,6 +12,7 @@ class App extends React.Component {
         lastName: '',
         email: '',
         password: '',
+        confirmPassword: '',
         mobileNo: '',
         formRadios: '',
         formCheck: '',
@@ -20,16 +21,18 @@ class App extends React.Component {
       },
       error: {
         firstName: false,
-        middleName:false,
+        middleName: false,
         lastName: false,
         email: false,
         password: false,
+        confirmPassword: false,
         mobileNo: false,
         formRadios: false,
         formCheck: false,
         formCheckHobby: false,
         dropDown: false
-      }
+      },
+      confirmPasswordError: 'Password is not valid'
     }
   }
 
@@ -43,7 +46,167 @@ class App extends React.Component {
     }, function () { this.validateField(name, value); }
     )
   }
-  
+
+  validateChecked = (e) => {
+    const { formCheckHobby } = this.state.form;
+    const { form } = this.state
+    const { name, value, checked } = e.target;
+    if (checked) {
+      formCheckHobby.push(value)
+    }
+    else {
+      let index = formCheckHobby.indexOf(value)
+      formCheckHobby.splice(index, 1)
+    }
+    this.setState({ form: { ...form, formCheckHobby: formCheckHobby } }, function () {
+      this.validateField(name, value);
+    })
+  }
+
+  validateField = (name, value) => {
+    switch (name) {
+      case "email":
+        console.log("hello")
+        const pattern = /[a-zA-Z0-9]+[\.]?([a-zA-Z0-9]+)?[\@][a-z]{3,9}[\.][a-z]{2,5}$/g;
+        const result = pattern.test(value);
+        if (result === false) { this.setState({ error: { ...this.state.error, [name]: true } }) }
+        else {
+          this.setState({ error: { ...this.state.error, [name]: false } })
+        }
+        break;
+      case "middleName": {
+        const pattern = /^[a-zA-Z]{1,10}$/;
+        const result = pattern.test(value);
+        if (result === false) { this.setState({ error: { ...this.state.error, [name]: true } }); }
+        else {
+          this.setState({ error: { ...this.state.error, [name]: false } })
+        }
+      }
+        break;
+      case "firstName": {
+        const pattern = /^[a-zA-Z]{1,10}$/;
+        const result = pattern.test(value);
+        if (result === false) { this.setState({ error: { ...this.state.error, [name]: true } }); }
+        else {
+          this.setState({ error: { ...this.state.error, [name]: false } })
+        }
+      }
+        break;
+      case "lastName": {
+        const pattern = /^[a-zA-Z]{1,10}$/;
+        const result = pattern.test(value);
+        if (result === false) { this.setState({ error: { ...this.state.error, [name]: true } }); }
+        else {
+          this.setState({ error: { ...this.state.error, [name]: false } })
+        }
+      }
+        break;
+      case "mobileNo": {
+        const pattern = /^[0-9]{10}$/;
+        const result = pattern.test(value);
+        if (result === false) { this.setState({ error: { ...this.state.error, [name]: true } }); }
+        else {
+          this.setState({ error: { ...this.state.error, [name]: false } })
+        }
+      }
+        break;
+      case "password": {
+        const pattern = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,32}$/;
+        const result = pattern.test(value);
+        if (result === false) { this.setState({ error: { ...this.state.error, [name]: true } }); }
+        else {
+          this.setState({ error: { ...this.state.error, [name]: false } })
+        }
+      }
+      break;
+      case "confirmPassword": {
+        const { password, confirmPassword } = this.state.form
+        const pattern = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,32}$/;
+        const result = pattern.test(value);
+        if (!result) {
+          if (password === confirmPassword) {
+            console.log("ans-true and result-false");
+            this.setState({
+              error: { ...this.state.error, [name]: true },
+              confirmPasswordError: "Enter password valid"
+            });
+          }
+          else {
+            console.log("ans-false and result-false");
+            this.setState({
+              error: { ...this.state.error, [name]: true },
+              confirmPasswordError: "Password and Confirm Password does not match"
+            });
+          }
+        }
+        else {
+          if (password === confirmPassword) {
+            console.log("ans-true and result-true");
+            this.setState({
+              error: { ...this.state.error, [name]: true },
+              confirmPasswordError: ""
+            })
+          }
+          else {
+            console.log("ans-false and result-true");
+            this.setState({
+              error: { ...this.state.error, [name]: false },
+              confirmPasswordError: 'Password and Confirm Password does not match'
+            })
+          }
+        }
+      }
+        break;
+      case "formRadios":
+        if (this.state.form.formRadios === '') {
+          this.setState({ error: { ...this.state.error, [name]: true } })
+        }
+        else {
+          this.setState({ error: { ...this.state.error, [name]: false } })
+        }
+        break;
+      case "formCheck":
+        if (this.state.form.formCheck === '') {
+          this.setState({ error: { ...this.state.error, [name]: true } });
+        }
+        else {
+          this.setState({ error: { ...this.state.error, [name]: false } });
+        }
+        break;
+      case "formCheckHobby":
+        const f = this.state.form.formCheckHobby;
+        if (f.length !== 0) {
+          this.setState({ error: { ...this.state.error, [name]: false } });
+        }
+        else {
+          this.setState({ error: { ...this.state.error, [name]: true } });
+        }
+        break;
+      case "dropDown":
+        if (this.state.form.dropDown === '') {
+          this.setState({ error: { ...this.state.error, [name]: true } });
+        }
+        else {
+          this.setState({ error: { ...this.state.error, [name]: false } });
+        }
+        break;
+      default:
+        console.log("default");
+    }
+  }
+  handleSubmit = (e) => {
+    let formError = this.state.error;
+    e.preventDefault()
+    const { form } = this.state
+    Object.keys(form).map(name => {
+      if (form[name] === "" ||
+        form[name].length === 0) {
+        formError[name] = true
+      }
+      return 0;
+    })
+    this.setState({ error: formError });
+  }
 
   render() {
     const isRequied = <><span className="error-msg">*</span></>
@@ -51,36 +214,45 @@ class App extends React.Component {
       <div className="container card">
         <Form onSubmit={(e) => this.handleSubmit(e)}>
           <Row>
-            <Col><Input title='First name' onChange={this.handleChange} name='firstName' isRequied={isRequied} error={this.state.error} form={this.state.form}/></Col>
-            {/* <Col><Input title='Middle name' name='middleName' onChange={this.handleChange}/></Col> */}
-            <Col><Input title='Last name' name='lastName' isRequied={isRequied}error={this.state.error} form={this.state.form} /></Col>
+            <Col><Input title='First name' onChange={this.handleChange} onBlur={this.handleChange} name='firstName' isRequied={isRequied} error={this.state.error} form={this.state.form} /></Col>
+            <Col><Input title='Middle name' name='middleName' isRequied={isRequied} onChange={this.handleChange} onBlur={this.handleChange} error={this.state.error} form={this.state.form} /></Col>
+            <Col><Input title='Last name' name='lastName' isRequied={isRequied} onChange={this.handleChange} onBlur={this.handleChange} error={this.state.error} form={this.state.form} /></Col>
           </Row>
           <Row>
-            <Col><Input title='Email' isRequied={isRequied} error={this.state.error} form={this.state.form}/></Col>
+            <Col><Input title='Email' name='email' isRequied={isRequied} error={this.state.error} onBlur={this.handleChange} onChange={this.handleChange} form={this.state.form} /></Col>
           </Row>
           <Row>
-            <Col><Input title='Mobile No.' isRequied={isRequied} error={this.state.error} form={this.state.form} /></Col>
-            <Col><Input title='Password' isRequied={isRequied} error={this.state.error} form={this.state.form}/></Col>
+            <Col><Input title='Mobile No.' name='mobileNo' isRequied={isRequied} error={this.state.error} onBlur={this.handleChange} onChange={this.handleChange} form={this.state.form} /></Col>
+
           </Row>
           <Row>
-            <Col md='6'><Input title='Gender' type='radio' label='Female' value='Female' name="formRadios" id='formFemale' isRequied={isRequied} error={this.state.error} form={this.state.form}/>
-              <Input type='radio' label='Male' value='Male' name="formRadios" id='formMale' error={this.state.error} form={this.state.form}/></Col>
-            <Col md='6'><Input title='Occupation' type="checkbox" value="Student" id="Student" label="Student" name="formCheck"error={this.state.error} form={this.state.form} />
-              <Input title='' type="checkbox" value="Employee" id="Employee" label="Employee" name="formCheck" error={this.state.error} form={this.state.form}/></Col>
+            <Col><Input title='Password' type="password" name='password' isRequied={isRequied} error={this.state.error} onChange={this.handleChange} onBlur={this.handleChange} form={this.state.form} /></Col>
+            <Col><Input title='Confirm Password' type="password" name='confirmPassword' isRequied={isRequied} error={this.state.error} onChange={this.handleChange} onBlur={this.handleChange} form={this.state.form} confirmPasswordError={this.state.confirmPasswordError} /></Col>
+          </Row>
+          <Row>
+            <Col >
+              <RadioButton title='Gender' type='radio' name='formRadios' onChange={this.handleChange} onBlur={this.handleChange} isRequied={isRequied} error={this.state.error} />
+            </Col>
           </Row>
           <Row>
             <Col>
-              <Input title='Hobby' type="checkbox" value="Singing" id="Singing" label="Singing" name="formCheckHobby" isRequied={isRequied} error={this.state.error} form={this.state.form} />
-              <Input type="checkbox" value="Dancing" id="Dancing" label="Dancing" name="formCheck" error={this.state.error} form={this.state.form} />
-              <Input type="checkbox" value="Reading" id="Reading" label="Reading" name="formCheck"  error={this.state.error} form={this.state.form}/>
-              <Input type="checkbox" value="Travelling" id="Travelling" label="Travelling" name="formCheck" error={this.state.error} form={this.state.form} />
+              <SelectOneCheckBox title='Occupation' type='Checkbox' name='formCheck' onChange={this.handleChange} onBlur={this.handleChange} error={this.state.error} form={this.state.form} isRequied={isRequied} />
             </Col>
           </Row>
           <Row>
-            <Col md='4'><Input title='City' type='dropDown' isRequied={isRequied} error={this.state.error} form={this.state.form} />
+            <Col>
+              <CheckBox title='Hobby' type='Checkbox' name="formCheckHobby" isRequied={isRequied} error={this.state.error} onBlur={this.handleChange} onChange={this.validateChecked} form={this.state.form} />
             </Col>
           </Row>
-          <Button type="submit">Submit form</Button>
+          <Row>
+            <Col md='4'>
+              <DropDown title='City' type='select' name='dropDown' isRequied={isRequied} onChange={this.handleChange} onBlur={this.handleChange} error={this.state.error} />
+            </Col>
+          </Row>
+          <Row>
+
+            <Col><Button type="submit">Submit form</Button></Col>
+          </Row>
         </Form>
       </div>
     );
--- src/Input.js
+++ src/Input.js
@@ -1,162 +1,291 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Form } from 'react-bootstrap';
-class Input extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      form: {
-        ...this.props.form
-      },
-      error: {
-        ...this.props.error,
-      }
 
+export default function Input(props) {
+  const { title, isRequied, name, type, value, onChange,onBlur, error,confirmPasswordError } = props;
 
-    }
+  if(name==='middleName'){
+    return (<Form.Group>
+      {title && <Form.Label>{title} {isRequied}</Form.Label>}
+      <Form.Control
+        name={name}
+        type={type}
+        value={value}
+        onChange={onChange}
+        onBlur={onBlur}
+      />
+      {error[name] ? <span className="error-msg">{`${title} is not valid.`}</span> : ''}
+    </Form.Group> )
   }
-  validateField = (name, value) => {
-    switch (name) {
-      case "email":
-        console.log("hello")
-        const pattern = /[a-zA-Z0-9]+[\.]?([a-zA-Z0-9]+)?[\@][a-z]{3,9}[\.][a-z]{2,5}$/g;
-        const result = pattern.test(value);
-        if (result === false) { this.setState({ error: { ...this.state.error, [name]: true } }) }
-        else {
-          this.setState({ error: { ...this.state.error, [name]: false } })
-        }
-        break;
-      case "firstName": {
-        const pattern = /^[a-zA-Z]{1,10}$/;
-        const result = pattern.test(value);
-        if (result === false) { this.setState({ error: { ...this.state.error, [name]: true } }); }
-        else {
-          this.setState({ error: { ...this.state.error, [name]: false } })
-        }
-      }
-        break;
-      case "lastName": {
-        const pattern = /^[a-zA-Z]{1,10}$/;
-        const result = pattern.test(value);
-        if (result === false) { this.setState({ error: { ...this.state.error, [name]: true } }); }
-        else {
-          this.setState({ error: { ...this.state.error, [name]: false } })
-        }
-      }
-        break;
-      case "mobileNo": {
-        const pattern = /^[0-9]{10}$/;
-        const result = pattern.test(value);
-        if (result === false) { this.setState({ error: { ...this.state.error, [name]: true } }); }
-        else {
-          this.setState({ error: { ...this.state.error, [name]: false } })
-        }
-      }
-        break;
-      case "password": {
-        const pattern = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,32}$/;
-        const result = pattern.test(value);
-        if (result === false) { this.setState({ error: { ...this.state.error, [name]: true } }); }
-        else {
-          this.setState({ error: { ...this.state.error, [name]: false } })
-        }
-      }
-        break;
-      case "formRadios":
-        if (this.state.form.formRadios === '') {
-          this.setState({ error: { ...this.state.error, [name]: true } })
-        }
-        else {
-          this.setState({ error: { ...this.state.error, [name]: false } })
-        }
-        break;
-      case "formCheck":
-        if (this.state.form.formCheck === '') {
-          this.setState({ error: { ...this.state.error, [name]: true } });
-        }
-        else {
-          this.setState({ error: { ...this.state.error, [name]: false } });
-        }
-        break;
-      case "formCheckHobby":
-        const f = this.state.form.formCheckHobby;
-        if (f.length !== 0) {
-          this.setState({ error: { ...this.state.error, [name]: false } });
-        }
-        else {
-          this.setState({ error: { ...this.state.error, [name]: true } });
-        }
-        break;
-      case "dropDown":
-        if (this.state.form.dropDown === '') {
-          this.setState({ error: { ...this.state.error, [name]: true } });
-        }
-        else {
-          this.setState({ error: { ...this.state.error, [name]: false } });
-        }
-        break;
-      default:
-        console.log("default");
-    }
+
+  else if(name==='confirmPassword'){
+    return (<Form.Group>
+      {title && <Form.Label>{title} {isRequied}</Form.Label>}
+      <Form.Control
+        name={name}
+        type={type}
+        value={value}
+        onChange={onChange}
+        onBlur={onBlur}
+      />
+      {error[name] ? <span className="error-msg">{confirmPasswordError}</span>:''}
+    </Form.Group> )
   }
-  render() {
-    const { type, value, name, placeholder, title, label, id, onChange, isRequied } = this.props;
-    const { error } = this.props
-    if (type === 'radio') {
-      Element = <>
-        <Form.Check type={type}
-          name={name}
-          label={label}
-          value={value}
-          id={id}
-          onChange={this.handleChange} /></>
-    }
-    else if (type === 'checkbox') {
-      Element = <>
-        <Form.Check type={type}
-          name={name}
-          label={label}
-          value={value}
-          id={id}
-          onChange={this.handleChange} /></>
-    }
-    else if (type === 'dropDown') {
-      Element = <>
-        <Form.Control as="select" name="dropDown" onChange={this.handleChange}>
-          <option name="dropDown" value="" label="select city" />
-          <option name="dropDown" value="Rajkot" label="Rajkot" />
-          <option name="dropDown" value="Ahemdabad" label="Ahemdabad" />
-          <option name="dropDown" value="Surat" label="Surat" />
-          <option name="dropDown" value="Vadodara" label="Vadodara" />
-        </Form.Control>
-      </>
-    }
-    else {
-      Element = <>
-        <Form.Control
-          type={type}
-          name={name}
-          label={label}
-          placeholder={placeholder}
-          onBlur={this.handleChange} /></>
-    }
-    return (
-      <Form.Group >
-        {title && <Form.Label> {title} {isRequied}</Form.Label>}
-        {Element}
-        {error[name] ? `${title} is requied.` : ''}
-      </Form.Group>
+
+  else {
+    return (<Form.Group>
+      {title && <Form.Label>{title} {isRequied}</Form.Label>}
+      <Form.Control
+        name={name}
+        type={type}
+        value={value}
+        onChange={onChange}
+        onBlur={onBlur}
+      />
+      {error[name] ? <span className="error-msg">{`${title} is requied.`}</span> : ''}
+    </Form.Group> 
     )
   }
 }
-Input.defaultProps = {
-  type: 'text',
-  label: '',
-  title: '',
-  isRequied: ''
-};
-Input.prototypes = {
-  type: PropTypes.number,
-  value: PropTypes.string
+
+export class CheckBox extends React.Component {
+  constructor() {
+    super()
+    this.Checkbox = [
+      { value: "Singing", label: "Singing" },
+      { value: "Dancing", label: "Dancing" },
+      { value: "Travelling", label: "Travelling" },
+      { value: "Reading", label: "Reading" },
+    ]
+  }
+  render() {
+    let { title, type, name, isRequied, onChange,onBlur, error } = this.props
+    return <><Form.Group>
+      {title && <Form.Label>{title} {isRequied}</Form.Label>}
+      {this.Checkbox.map((item, index) => {
+        return (<div key={index}>
+          <Form.Check type={type}
+            name={name}
+            value={item.value}
+            onChange={onChange}
+            onBlur={onBlur}
+            label={item.label}
+            error={error} /></div >
+        );
+      })}
+      {error[name] ? <span className="error-msg">{`${title} is requied.`}</span> : ''}
+    </Form.Group>
+    </>
+  }
 }
-export default Input;
+
+export class RadioButton extends React.Component {
+  constructor() {
+    super()
+    this.gender = [
+      { value: "female", label: "Female", name: "gender" },
+      { value: "male", label: "Male", name: "gender" },
+    ]
+  }
+  render() {
+    let { title, type, isRequied,name,error,onChange,onBlur } = this.props
+    return <><Form.Group>
+      {title && <Form.Label>{title} {isRequied}</Form.Label>}
+      {this.gender.map((item, index) => {
+        return (<div key={index}>
+          <Form.Check type={type}
+            label={item.label}
+            onChange={onChange}
+            onBlur={onBlur}
+            value={item.value}
+            name={item.name} />
+        </div>)
+      })}
+      {error[name] ? <span className="error-msg">{`${title} is requied.`}</span> : ''}
+    </Form.Group>
+    </>
+  }
+}
+
+export class SelectOneCheckBox extends React.Component {
+  constructor() {
+    super()
+    this.occupation = [
+      { value: "Employee", label: "Employee", id: "formEmployee" },
+      { value: "Student", label: "Student", id: "formStudent" },
+    ]
+  }
+  render() {
+    let { error, title, type, name, isRequied, onChange, form } = this.props
+    return <> <Form.Group>
+      {title && <Form.Label>{title} {isRequied}</Form.Label>}
+      {this.occupation.map((item, index) => {
+        return (<div key={index}>
+          <Form.Check
+            type={type}
+            name={name}
+            id={item.id}
+            value={item.value}
+            label={item.label}
+            onChange={onChange}
+            checked={form.formCheck === item.value}
+          />
+        </div>
+        );
+      })}
+      {error[name] ? <span className="error-msg">{`${title} is requied.`}</span> : ''}
+    </Form.Group>
+    </>
+  }
+}
+
+export class DropDown extends React.Component {
+  constructor() {
+    super();
+    this.option = [
+      { value: "Rajkot", label: "Rajkot" },
+      { value: "Ahemdabad", label: "Ahemdabad" },
+      { value: "Surendranagar", label: "Surendranagar" },
+      { value: "Vadodara", label: "Vadodara" },
+    ]
+  }
+  render() {
+    let { title, type, name, onChange, isRequied, error } = this.props;
+    return (<Form.Group>
+      {title && <Form.Label>{title} {isRequied}</Form.Label>}
+      <Form.Control as={type}
+        name={name}
+        onChange={onChange}>
+        <option>Select City</option>
+        {this.option.map((item, index) => {
+          return (
+            <option
+              label={item.label}
+              value={item.value} />
+         )
+          
+        })
+        }
+      </Form.Control>
+      {error[name] ? <span className="error-msg">{`${title} is requied.`}</span> : ''}
+    </Form.Group>
+    )
+  }
+}
+CheckBox.defaultProps = {
+  type: "checkbox"
+}
+
+SelectOneCheckBox.defaultProps = {
+  type: "checkbox"
+}
+RadioButton.defaultProps = {
+  type: "radio"
+}
+
+Input.defaultProps = {
+  type: "text",
+  style: {
+    margin: "10px",
+  },
+  isRequired: false,
+}
+
+Input.propTypes = {
+  name: PropTypes.string,
+  type: PropTypes.string,
+  value: PropTypes.string,
+  style: PropTypes.object,
+  placeholder: PropTypes.string
+}
+// class Input extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       form: {
+//         ...this.props.form
+//       },
+//       error: {
+//         ...this.props.error,
+//       }
+//     }
+//   }
+//   render() {
+//     const { type, value, name, placeholder, title, label, id, onChange, isRequied } = this.props;
+//     const { error } = this.props;
+//     const Checkbox = [
+//       {
+//         name: 'Singing',
+//         label: 'Singing',
+//         value: 'Singing'
+//       },
+//       {
+//         name: 'Dancing',
+//         label: 'Dancing',
+//         value: 'Dancing'
+//       },
+//       {
+//         name: 'Travelling',
+//         label: 'Travelling',
+//         value: 'Travelling'
+//       },
+//       {
+//         name: 'Reading',
+//         label: 'Reading',
+//         value: 'Reading'
+//       }
+//     ]
+//     if (type === 'radio') {
+//       Element = <>
+//         <Form.Check type={type}
+//           name={name}
+//           label={label}
+//           value={value}
+//           id={id}
+//           onChange={onChange} /></>
+//     }
+//     else if (type === 'checkbox') {
+//       Element = <>
+//         {Checkbox.map((item, index) => <Form.Check type={type} name={item.name} label={item.label} value={item.value} />)}
+//       </>
+//     }
+//     else if (type === 'dropDown') {
+//       Element = <>
+//         <Form.Control as="select" value={value} name="dropDown" onChange={onChange}>
+//           <option name="dropDown" value="" label="select city" />
+//           <option name="dropDown" value="Rajkot" label="Rajkot" />
+//           <option name="dropDown" value="Ahemdabad" label="Ahemdabad" />
+//           <option name="dropDown" value="Surat" label="Surat" />
+//           <option name="dropDown" value="Vadodara" label="Vadodara" />
+//         </Form.Control>
+//       </>
+//     }
+//     else {
+//       Element = <>
+//         <Form.Control
+//           type={type}
+//           name={name}
+//           label={label}
+//           placeholder={placeholder}
+//           onBlur={onChange} /></>
+//     }
+//     return (<Form.Group >
+//       {title && <Form.Label>{title} {isRequied}</Form.Label>}
+//       {Element}
+//       {error[name] ? <span className="error-msg">`${title} is requied.`</span> : ''}
+//     </Form.Group>
+//     )
+//   }
+// }
+// Input.defaultProps = {
+//   type: 'text',
+//   label: '',
+//   title: '',
+//   isRequied: ''
+// };
+// Input.prototypes = {
+//   type: PropTypes.number,
+//   value: PropTypes.string
+// }
+// export default Input;
